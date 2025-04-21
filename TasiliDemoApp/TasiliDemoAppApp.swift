164fa8f1c773782306c667fa3ba36871ad384245
Haniefa Binsaad <haniefabinsaad@gmail.com>
2025-04-21 12:31:13 +0300

    Initial Commit

---
+++ TasiliDemoApp/TasiliDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasiliDemoAppApp.swift
+//  TasiliDemoApp
+//
+//  Created by Haniefa Binsaad on 2025/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TasiliDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasiliDemoAppTests/TasiliDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasiliDemoAppTests.swift
+//  TasiliDemoAppTests
+//
+//  Created by Haniefa Binsaad on 2025/04/21.
+//
+
+import Testing
+@testable import TasiliDemoApp
+
+struct TasiliDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
